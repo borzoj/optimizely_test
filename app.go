10d8445d01c0d5516612401e16ac7ba2054dbb7c
Michal Jaworski <borzoj@gmail.com>
2020-03-23 21:56:19 +0000

    test

---
+++ app.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+
+	"github.com/optimizely/go-sdk/pkg/client"
+	"github.com/optimizely/go-sdk/pkg/entities"
+	"github.com/optimizely/go-sdk/pkg/logging"
+)
+
+const optimizeSdkKey = "XWrur15ApwRCYRASkqUmA"
+
+var datafileURL = "https://cdn.optimizely.com/datafiles/XWrur15ApwRCYRASkqUmA.json"
+
+func randString() string {
+	return strconv.Itoa(int(rand.Float64() * 100000))
+}
+
+func main() {
+
+	//rand.Seed(time.Now().UTC().UnixNano())
+	logging.SetLogLevel(logging.LogLevelError)
+
+	optimizelyFactory := &client.OptimizelyFactory{
+		SDKKey: optimizeSdkKey,
+	}
+
+	// Instantiates a client that syncs the datafile in the background
+	optimizelyClient, err := optimizelyFactory.Client()
+
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i <= 10; i++ {
+		user := entities.UserContext{
+			ID: "michal+" + randString() + "@zavamed.com",
+		}
+
+		isEnabled, err := optimizelyClient.IsFeatureEnabled("michal_test", user)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("%s: %t\n", user.ID, isEnabled)
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module mymodule
+
+go 1.12
+
+require (
+	github.com/optimizely/go-sdk v1.0.0-rc1
+	github.com/pkg/errors v0.9.1 // indirect
+)
